1e7b97297503021a2d73d6b84e5202b79be933b8
onooff <onooff@naver.com>
2021-11-24 15:18:01 +0000

    시간 여행 구현하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -71,44 +71,13 @@ class Game extends React.Component {
 		super(props);
 		this.state = {
 			history: [{ squares: Array(9).fill(null) }],
+			stepNumber: 0,
 			xIsNext: true,
 		};
 	}
-	render() {
-		const history = this.state.history;
-		const current = history[history.length - 1];
-		const winner = calculateWinner(current.squares);
-
-		const moves = history.map((step, move) => {
-			const desc = move ? move + '번 턴으로 이동' : '게임 시작으로 이동';
-			return (
-				<li>
-					<button onClick={() => this.jumpTo(move)}>{desc}</button>
-				</li>
-			);
-		});
-
-		let status;
-		if (winner) {
-			status = 'Winner: ' + winner;
-		} else {
-			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-		}
-		return (
-			<div className="game">
-				<div className="game-board">
-					<Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
-				</div>
-				<div className="game-info">
-					<div>{status}</div>
-					<ol>{moves}</ol>
-				</div>
-			</div>
-		);
-	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		/*
@@ -134,8 +103,49 @@ class Game extends React.Component {
 			주의
 			배열 push() 함수와 같이 더 익숙한 방식과 달리 concat() 함수는 기존 배열을 변경하지 않기 때문에 이를 더 권장합니다.
 			*/
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
 		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
+		});
+	}
+
+	render() {
+		const history = this.state.history;
+		const current = history[this.state.stepNumber];
+		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ? move + '번 턴으로 이동' : '게임 시작으로 이동';
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
+		let status;
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
+				</div>
+				<div className="game-info">
+					<div>{status}</div>
+					<ol>{moves}</ol>
+				</div>
+			</div>
+		);
 	}
 }
 
