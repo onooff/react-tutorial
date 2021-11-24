1ab1c5e73b6cb8f4a7534b5974571baf8d759e24
onooff <onooff@naver.com>
2021-11-24 15:06:36 +0000

    과거의 이동 표시하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -78,6 +78,16 @@ class Game extends React.Component {
 		const history = this.state.history;
 		const current = history[history.length - 1];
 		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ? move + '번 턴으로 이동' : '게임 시작으로 이동';
+			return (
+				<li>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
 		let status;
 		if (winner) {
 			status = 'Winner: ' + winner;
@@ -91,7 +101,7 @@ class Game extends React.Component {
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
-					<ol>{/* TODO */}</ol>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
