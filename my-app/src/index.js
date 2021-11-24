8f951405121956cbcc06cb819acba68e3d3aba97
onooff <onooff@naver.com>
2021-11-24 14:03:08 +0000

    승자 결정하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -40,6 +40,7 @@ class Board extends React.Component {
 
 	handleClick(i) {
 		const squares = this.state.squares.slice();
+		if (calculateWinner(squares) || squares[i]) return;
 		/*
 		 .slice() 사용한 이유?
 		 state의 squares를 직접 건드리지 않고 사본을 생성하기 위함. 왜???????
@@ -64,7 +65,13 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		const winner = calculateWinner(this.state.squares);
+		let status;
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
 
 		return (
 			<div>
@@ -103,6 +110,26 @@ class Game extends React.Component {
 			</div>
 		);
 	}
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 // ========================================
