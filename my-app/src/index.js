43afd0fc38f139f90a7815852bf4fdec3f39334e
onooff <onooff@naver.com>
2021-11-24 13:58:21 +0000

    순서 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -34,6 +34,7 @@ class Board extends React.Component {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
 
@@ -50,8 +51,11 @@ class Board extends React.Component {
 		   순수 컴포넌트?
 		   https://ko.reactjs.org/docs/optimizing-performance.html#examples
 		*/
-		squares[i] = 'X';
-		this.setState({ squares: squares });
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
 	}
 
 	renderSquare(i) {
@@ -60,7 +64,7 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = 'Next player: X';
+		const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
 		return (
 			<div>
